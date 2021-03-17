e8ce583c5643377bc3fb013db8934e0f119f2813
Pedro Paulo Bazzo <pedropbazzo@outlook.com>
2021-03-17 20:42:49 -0300

    feat: new test issue

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -6,6 +6,7 @@ import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 @Injectable()
 export class DataService {
   private readonly API_URL = 'https://api.github.com/repos/angular/angular/issues';
+  // Add new api issue for test
 
   dataChange: BehaviorSubject<Issue[]> = new BehaviorSubject<Issue[]>([]);
   // Temporarily stores data from dialogs
